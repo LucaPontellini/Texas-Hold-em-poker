bf39a6234a7219957b91c6cd5b90ebbe19ce801d
LucaPontellini <147493566+LucaPontellini@users.noreply.github.com>
2025-01-25 15:39:50 +0100

    edit

--- texas_hold_em_poker.py
+++ texas_hold_em_poker.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append('Texas-Hold-em-poker/python_files')
+sys.path.append('f:/Texas-Hold-em-poker/python_files')  # Percorso corretto per il modulo
 
 from flask import Flask, render_template, request, jsonify
 from game import Game
